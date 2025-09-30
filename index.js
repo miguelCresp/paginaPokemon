091e46bd783a90d446b7493b6e16b4f15ad70cf8
Miguel Crespo <miguelccpazos@gmail.com>
2025-09-30 09:45:41 +0200

    subir

---
+++ index.js
@@ -0,0 +1,24 @@
+const pokemon = "psyduck";
+const statsList = document.getElementById("stats");
+const imagen = document.getElementById("imagen");
+const ruido = document.getElementById("ruido");
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const stats = data.stats;
+    stats.forEach((stat) => {
+      const statLi = document.createElement("li");
+      statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+      statsList.appendChild(statLi);
+    });
+    const img = document.createElement("img");
+    img.src = data.sprites.front_default;
+    img.alt = pokemon;
+    imagen.appendChild(img);
+
+    const cry = data.cries.latest;
+    ruido.src = cry;
+  })
+
+  .catch((error) => console.error("Error al obtener los datos:", error));
