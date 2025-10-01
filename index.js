af160505cd240e525474e0b057650cfe43a2d063
Miguel Crespo <miguelccpazos@gmail.com>
2025-10-01 13:48:07 +0200

    campo de texto

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const pokemon = "psyduck";
 const statsList = document.getElementById("stats");
 const imagen = document.getElementById("imagen");
 const ruido = document.getElementById("ruido");
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  .then((response) => response.json())
-  .then((data) => {
-    const stats = data.stats;
-    stats.forEach((stat) => {
-      const statLi = document.createElement("li");
-      statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
-      statsList.appendChild(statLi);
-    });
-    const img = document.createElement("img");
-    img.src = data.sprites.front_default;
-    img.alt = pokemon;
-    imagen.appendChild(img);
+form.addEventListener("submit", (campo) => {
+  campo.preventDefault();
+  const pokemon = input.value.toLowerCase().trim();
+  if (!pokemon) return;
 
-    const cry = data.cries.latest;
-    ruido.src = cry;
-  })
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const stats = data.stats;
+      stats.forEach((stat) => {
+        const statLi = document.createElement("li");
+        statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+        statsList.appendChild(statLi);
+      });
+      const img = document.createElement("img");
+      img.src = data.sprites.front_default;
+      img.alt = pokemon;
+      imagen.appendChild(img);
 
-  .catch((error) => console.error("Error al obtener los datos:", error));
+      const cry = data.cries.latest;
+      ruido.src = cry;
+    })
+
+    .catch((error) => console.error("Error al obtener los datos:", error));
+});
